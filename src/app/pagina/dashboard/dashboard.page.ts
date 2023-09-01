4abf205372d0e25eba546d4b0a0a4dfb3dacbb49
ValterMonteiro <valtinms@gmail.com>
2023-09-01 09:12:32 -0400

    trantando dados recebidos do back-end

--- src/app/pagina/dashboard/dashboard.page.ts
+++ src/app/pagina/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GestaoE_Service } from 'src/app/gestaoe-back-end.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  produtos: any[] = []; // Defina o tipo de dados de acordo com a estrutura dos seus produtos
+
+  constructor(private gestaoe_produtos: GestaoE_Service) {
+    /* this.getDados(); */
+   }
+
+  /* getDados() {
+    this.gestaoe_produtos.getDados().subscribe(data => {
+      console.log(data);
+    });
+  } */
 
   ngOnInit() {
+    this.gestaoe_produtos.getDados().subscribe(data => {
+        this.produtos = data; // Supondo que o serviço retorna uma lista de produtos
+      },
+      (error) => {
+        console.error('Erro ao buscar dados:', error);
+      }
+    );
   }
 
 }
