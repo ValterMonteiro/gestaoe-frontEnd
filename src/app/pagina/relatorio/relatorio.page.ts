60f6b6e5aa49f2d33c264f404bbc779c5d6f64cb
ValterMonteiro <valtinms@gmail.com>
2023-09-17 10:53:44 -0400

    atualizando relatorio

--- src/app/pagina/relatorio/relatorio.page.ts
+++ src/app/pagina/relatorio/relatorio.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { RelatorioDTO } from 'src/app/models/relatorio.dto';
+import { RelatorioService } from 'src/app/services/domain/relatorio.service';
+/* import { GestaoE_Service } from 'src/app/gestaoe-back-end.service'; */
 
 @Component({
   selector: 'app-relatorio',
@@ -7,14 +10,33 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./relatorio.page.scss'],
 })
 export class RelatorioPage implements OnInit {
+  relatorioData: any[] = [];
+  data: any[] = []; // Armazene os dados originais aqui
+  results: any[] = []; // Armazene os resultados da pesquisa aqui
 
-  constructor(public nav: NavController) { }
+
+  relatorio!: RelatorioDTO[];
+
+  constructor(public nav: NavController/* , private gestaoe_produtos: GestaoE_Service */,
+  public relatorioService: RelatorioService) { }
+
+//findall().subscribe(res => {}, err => {})
+ionViewDidEnter(){
+  this.relatorioService.findAll()
+                         .subscribe(response => {
+                            this.relatorio = response;
+                            console.log(response);
+                         }, error => {
+                            console.log(error);
+                         });
+}
 
   abrirPagina(x: string) {
     this.nav.navigateForward(x);
   }
 
   ngOnInit() {
+
   }
 
 }
