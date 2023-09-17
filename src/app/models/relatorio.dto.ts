09e583aad73755ce5d6c695d41b9078cbca086bd
ValterMonteiro <valtinms@gmail.com>
2023-09-17 10:53:10 -0400

    criando relatorio.service e relatorio.dto

---
+++ src/app/models/relatorio.dto.ts
@@ -0,0 +1,14 @@
+export interface RelatorioDTO {
+  data: string;
+  id_produto: number;
+  descricao: string;
+  entrada_quantidade: number;
+  entrada_valor_unit: number;
+  entrada_valor_total: number;
+  saida_quantidade: number;
+  saida_valor_unit: number;
+  saida_valor_total: number;
+  saldo_quantidade: number;
+  saldo_valor_medio: number;
+  saldo_valor_total: number;
+  }
---
+++ src/app/services/domain/relatorio.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { API_CONFIG } from "src/app/config/api.config";
+import { RelatorioDTO } from "src/app/models/relatorio.dto";
+
+
+@Injectable()
+export class RelatorioService {
+
+  constructor(public http: HttpClient) {
+  }
+
+  //Iniciando a consulta na API
+  //Crie um método findAll() e passe a URL do mapeamento
+  findAll(): Observable<RelatorioDTO[]> {
+    return this.http.get<RelatorioDTO[]>(`${API_CONFIG.baseUrl}/relatorio`)
+  }
+}
+
+
+
