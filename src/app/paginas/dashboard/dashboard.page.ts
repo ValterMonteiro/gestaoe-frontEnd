2039f356a89029a8d110dd869abbd4b9c8487dc9
ValterMonteiro <valtinms@gmail.com>
2023-11-18 18:15:36 -0400

    inserindo gráficos e atualizando dashboard

--- src/app/paginas/dashboard/dashboard.page.ts
+++ src/app/paginas/dashboard/dashboard.page.ts
@@ -1,6 +1,6 @@
 import { query } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { ChartConfiguration, ChartDataset, ChartOptions, ChartType, LabelItem } from 'chart.js';
@@ -17,50 +17,20 @@ import { RelatorioService } from 'src/app/services/domain/relatorio.service';
 })
 export class DashboardPage implements OnInit {
 
-  data: any[] = [];
-  results: any[] = [];
-  resultsById: any[] = [];
-
   produtos!: ProdutoDTO[];
   relatorio!: RelatorioDTO[];
-
-
-  /* dashboard teste */
-  public chartData: ChartDataset[] = [{data: [], label: 'Compras'}, {data: [], label: 'Vendas'}];
-  public chartType: ChartType = 'line';
-  /* public chartLabels: Label[]; */
-  public chartLabels: Array<any> = [];
-
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July'
-    ],
-    datasets: [
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Compras',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
-      }
-    ]
-  };
-  public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false
-  };
-  public lineChartLegend = true;
-
-  /*fim dashboard teste */
-
-  /* Variáveis */
+  chartData: ChartDataset[] = [
+    { data: [], label: 'Compras' },
+    { data: [], label: 'Vendas' }
+  ];
+  chartDataCompras: ChartDataset[] = [{ data: [], label: 'Compras' }];
+  chartDataVendas: ChartDataset[] = [{ data: [], label: 'Vendas' }];
+  chartDataGiroDeEstoque: ChartDataset[] = [];
+  chartType: ChartType = 'line';
+  chartLabels: Array<any> = [];
   id!: number;
+  custoDasVendas!: number;
+  estoqueMedio!: number;
 
   constructor(
     public relatorioService: RelatorioService,
@@ -68,11 +38,10 @@ export class DashboardPage implements OnInit {
     public navController: NavController,
     private route: ActivatedRoute,
     private httpClient: HttpClient) {
-      this.localData();
-      this.id = Number(this.route.snapshot.paramMap.get('id')) || 0;
-      console.log('ID dashboard:', this.id);
-      this.chartLabels = [];
-     }
+    this.localData();
+    this.id = Number(this.route.snapshot.paramMap.get('id')) || 0;
+    console.log('ID dashboard:', this.id);
+  }
 
   ionViewDidEnter() {
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
@@ -85,13 +54,74 @@ export class DashboardPage implements OnInit {
         console.log(error);
       });
 
-      this.produtoService.findAll()
+    this.produtoService.findAll()
       .subscribe({
         next:
           (response) => this.produtos = response,
         error:
           (error) => console.log(error)
       });
+  }
+
+  ngOnInit() {
+    /* const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    console.log("O ID passado é " + id);
+    this.relatorioService.findAll(id)
+      .subscribe(response => {
+        this.relatorio = response;
+        console.log(response);
+      }, error => {
+        console.log(error);
+      }); */
+  }
+
+  localData() {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.relatorioService.findAll(id)
+      .subscribe(
+        response => {
+          this.chartLabels = [];
+          this.chartData = [];
+          this.chartDataCompras = [];
+          this.chartDataVendas = [];
+          this.chartDataGiroDeEstoque = [];
+          this.calculateStockTurnover();
+          this.chartData.push({ data: [], label: 'Compras' }, { data: [], label: 'Vendas' });
+          this.chartDataCompras.push({ data: [], label: 'Compras' });
+          this.chartDataVendas.push({ data: [], label: 'Vendas' });
+
+          // Agora você pode usar os dados do relatório como necessário
+          for (let i = 0; i < response.length; i++) {
+            /* console.log(response[i].data); */
+            this.chartLabels.push(response[i].data);
+
+            /* grafico de compras e vendas */
+            this.chartData[0].data.push(response[i].entradaQuantidade);
+            this.chartData[1].data.push(response[i].saidaQuantidade);
+
+            /* grafico de compras */
+            this.chartDataCompras[0].data.push(response[i].entradaQuantidade);
+
+            /* grafico de vendas */
+            this.chartDataVendas[0].data.push(response[i].saidaQuantidade);
+
+          }
+        }, error => {
+          console.log(error);
+        });
+  }
+
+  calculateStockTurnover() {
+    const estoqueInicial = 5000; // Valor de exemplo
+    const estoqueFinal = 7000; // Valor de exemplo
+    this.custoDasVendas = 15000; // Valor de exemplo
+    this.estoqueMedio = (estoqueInicial + estoqueFinal) / 2;
+
+    const giroDeEstoque = this.custoDasVendas / this.estoqueMedio;
+    console.log('Giro de Estoque:', giroDeEstoque);
+
+    // Agora, vamos inserir esse valor no gráfico de giro de estoque
+    this.chartDataGiroDeEstoque.push({ data: [giroDeEstoque], label: 'Giro de Estoque' });
   }
 
   relatorioProduto(id: number) {
@@ -102,52 +132,14 @@ export class DashboardPage implements OnInit {
     this.navController.navigateForward('sel-produto');
   }
 
-  ngOnInit() {
-    const id: number = Number(this.route.snapshot.paramMap.get('id'));
-    console.log("O ID passado é " + id);
-    this.relatorioService.findAll(id)
-      .subscribe(response => {
-        this.relatorio = response;
-        console.log(response);
-      }, error => {
-        console.log(error);
-      });
-   }
+  /* calculo do estoque médio */
+  /* calcularEstoqueMedio(estoqueInicial: number, estoqueFinal: number): number {
+    return (estoqueInicial + estoqueFinal) / 2;
+  } */
 
-  localData() {
-    /* const request: string =
-
-    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=PETR4.SA&interval=5min&apikey=4594F3S46TWWNI95`;
-
-    this.httpClient.get(request).subscribe(res => {
-      console.log(res);
-      const data: any = (res as any).data;
-      for (let i = 0; i < data; i++) {
-        console.log(data[i].symbol);
-      }
-    }); */
-
-    const id: number = Number(this.route.snapshot.paramMap.get('id'));
-    this.relatorioService.findAll(id)
-      .subscribe(response => {
-        /* console.log(response); */
-
-        this.chartLabels = [];
-        this.chartData[0].data = [];
-
-        // Agora você pode usar os dados do relatório como necessário
-      for (let i = 0; i < response.length; i++) {
-
-        /* console.log(response[i].data); */
-        this.chartLabels.push(response[i].data);
-        this.chartData[0].data.push(response[i].entradaQuantidade);
-        this.chartData[1].data.push(response[i].saidaQuantidade);
-
-      }
-      }, error => {
-        console.log(error);
-      });
-
-  }
+  // Exemplo de utilização
+  /* const estoque = new DashboardPage(15000, 5000, 20000, 7000); // Valores de exemplo
+  const giroDeEstoque = estoque.calcularGiroDeEstoque();
+console.log("Giro de Estoque:", giroDeEstoque); */
 
 }
